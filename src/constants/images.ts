bb052a2ed59aafeb7187332ab2e86732ad5da40d
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-09-06 20:18:43 +0900

    feat: 랜딩 페이지 FeatureSection 이미지 애셋 변경

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,6 +3,6 @@ export const DOWNLOAD_ON_APP_STORE = "/images/download_on_app_store.png";
 export const DOWNLOAD_ON_PLAY_STORE = "/images/download_on_google_play.png";
 
 export const APP_SCREEN_1 = "/images/app_screen_1.png";
-export const APP_SCREEN_2 = "/images/app_screen_1.png";
-export const APP_SCREEN_3 = "/images/app_screen_1.png";
-export const APP_SCREEN_4 = "/images/app_screen_1.png";
+export const APP_SCREEN_2 = "/images/app_screen_2.png";
+export const APP_SCREEN_3 = "/images/app_screen_3.png";
+export const APP_SCREEN_4 = "/images/app_screen_4.png";
